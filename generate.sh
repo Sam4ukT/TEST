fc70adc37d0fbfc9e532c21ee227caa87055667d
Sam4uk <sto.ukr.net@gmail.com>
2023-04-08 21:26:56 +0300

    субота, 8 квітня 2023 21:26:56 +0300TEST3

---
+++ generate.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+SHA256=${1}
+FILE_NAME=${2}
+
+
+
+OUT_FILE="package_NanitCore_index.json"
+HOST="https://sam4ukt.github.io"
+FILE_SIZE=$(stat -c %s ${FILE_NAME})
+# SHA256=$(sha256sum ${FILE_NAME})
+FILE_PATH="TEST"
+
+# sha256sum Nanit.zip
+
+cat <<EOF >"${OUT_FILE}"
+{
+  "packages": [
+    {
+      "name": "Nanit",
+      "maintainer": "NanitRobots",
+      "websiteURL": "",
+      "email": "",
+      "help": {
+        "online": ""
+      },
+      "platforms": [
+        {
+          "name": "Nanit",
+          "architecture": "avr",
+          "version": "0.0.3",
+          "category": "Contributed",
+          "help": {
+            "online": ""
+          },
+          "url": "${HOST}/${FILE_PATH}/${FILE_NAME}",
+          "archiveFileName": "${FILE_NAME}",
+          "checksum" : "SHA-256:${SHA256}",
+          "size" : "${FILE_SIZE}",
+          "boards": [
+            {
+              "name": "Nanit Discovery 2.0"
+            },
+            {
+              "name": "Nanit Discovery 3"
+            },
+            {
+              "name": "Nanit Discovery 3.1"
+            },
+            {
+              "name": "Nanit Smart Home (Nanit 3.1 based)"
+            },
+            {
+              "name": "Nanit Smart Home (Nanit 3.22 based)"
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
+EOF
